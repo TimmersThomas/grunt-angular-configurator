3a4294045d0c1e168f3f75daf9eadc28bc2820d3
Thomas Timmers <thomas@cleardigital.be>
2014-10-20 09:07:59 +0200

    Using a different method for saving file

--- tasks/configurator.js
+++ tasks/configurator.js
@@ -14,106 +14,109 @@ var _ = require('lodash'),
     fs = require('fs');
 
 function generateTemplateSimple() {
-  var content = "'use strict';\n" +
-                "angular.module('<%= module %>')\n" +
-                "    .constant('<%= variable %>', <%= config %>);";
+    var content = "'use strict';\n" +
+        "angular.module('<%= module %>')\n" +
+        "    .constant('<%= variable %>', <%= config %>);";
 
-  return content;
+    return content;
 };
 
 function generateTemplateBrowserified() {
-  var content = "'use strict';\n\n" +
-                "exports.inject = function (appModule) {\n" +
-                "   appModule.constant('<%= variable %>', exports.constant);\n" +
-                "   return exports.constant;\n" +
-                "};\n\n" +
-                "exports.constant = <%= config %>";
+    var content = "'use strict';\n\n" +
+        "exports.inject = function (appModule) {\n" +
+        "   appModule.constant('<%= variable %>', exports.constant);\n" +
+        "   return exports.constant;\n" +
+        "};\n\n" +
+        "exports.constant = <%= config %>";
 
-  return content;
+    return content;
 };
 
 
-function generateConfiguration(grunt, options){
-  var files = grunt.file.expand({ filter: 'isDirectory'}, options.search_base + '*');
+function generateConfiguration(grunt, options) {
+    var files = grunt.file.expand({filter: 'isDirectory'}, options.search_base + '*');
 
-  var config = {},
-      states = {};
+    var config = {},
+        states = {};
 
-  grunt.log.debug("Reading files: " + JSON.stringify(files));
-  _.forEach(files, function(dir) {
-      var module = path.basename(dir),
-          configFile = path.resolve(dir, options.config_folder, options.config_file_config),
-          stateFile = path.resolve(dir, options.config_folder, options.config_file_state);
+    grunt.log.debug("Reading files: " + JSON.stringify(files));
+    _.forEach(files, function (dir) {
+        var module = path.basename(dir),
+            configFile = path.resolve(dir, options.config_folder, options.config_file_config),
+            stateFile = path.resolve(dir, options.config_folder, options.config_file_state);
 
-      if (fs.existsSync(configFile)) {
-          grunt.log.debug("File exists: " + configFile);
-          _.merge(config, grunt.file.readJSON(configFile));
-      }
+        if (fs.existsSync(configFile)) {
+            grunt.log.debug("File exists: " + configFile);
+            _.merge(config, grunt.file.readJSON(configFile));
+        }
 
-      if (fs.existsSync(stateFile)) {
-          grunt.log.debug("File exists: " + stateFile);
+        if (fs.existsSync(stateFile)) {
+            grunt.log.debug("File exists: " + stateFile);
 
-          _.merge(states, {states: _.mapValues(grunt.file.readJSON(stateFile), function(state) {
-              return _.extend({
-                  'module': module
-              }, state);
-          })});
-      }
-  });
-
-  config = _.merge(
-              {},
-              states,
-              config,
-              grunt.file.readJSON( path.resolve(options.general_config_folder, 'config.json') ),
-              grunt.file.readJSON( path.resolve(options.general_config_folder, 'config_' + options.env + '.json') )
-            );
-
-  grunt.log.debug("Generated Config: " + JSON.stringify(config));
-  return config;
-}
-
-module.exports = function(grunt) {
-
-  grunt.registerTask('ng_config_configurator', 'Generate a configuration based on the current ENVIRONMENT', function(envTarget) {
-    var environment = envTarget || grunt.config('env');
-
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      "env": environment,
-      "general_config_folder": "app/scripts/_config/",
-      "search_base": "app/scripts/",
-      "config_folder": "config",
-      "config_file_config": "config.json",
-      "config_file_state": "states.json",
-      "export_module": "app.common",
-      "export_variable": "config",
-      "export_dest": "app/scripts/common/config/config.js",
-      "browserify": true
+            _.merge(states, {
+                states: _.mapValues(grunt.file.readJSON(stateFile), function (state) {
+                    return _.extend({
+                        'module': module
+                    }, state);
+                })
+            });
+        }
     });
 
-    grunt.log.debug("Using options: " + JSON.stringify(options));
+    config = _.merge(
+        {},
+        states,
+        config,
+        grunt.file.readJSON(path.resolve(options.general_config_folder, 'config.json')),
+        grunt.file.readJSON(path.resolve(options.general_config_folder, 'config_' + options.env + '.json'))
+    );
 
-    var template = generateTemplateSimple(),
-        configuraton = generateConfiguration(grunt, options);
+    grunt.log.debug("Generated Config: " + JSON.stringify(config));
 
-    if(options.browserify){
-      template = generateTemplateBrowserified();
-    }
+    return config;
+}
 
-    grunt.log.debug("Used template: " + template);
+module.exports = function (grunt) {
+
+    grunt.registerTask('ng_config_configurator', 'Generate a configuration based on the current ENVIRONMENT', function (envTarget) {
+        var environment = envTarget || grunt.config('env');
+
+        // Merge task-specific and/or target-specific options with these defaults.
+        var options = this.options({
+            "env": environment,
+            "general_config_folder": "app/scripts/_config/",
+            "search_base": "app/scripts/",
+            "config_folder": "config",
+            "config_file_config": "config.json",
+            "config_file_state": "states.json",
+            "export_module": "app.common",
+            "export_variable": "config",
+            "export_dest": "app/scripts/common/config/config.js",
+            "browserify": true
+        });
+
+        grunt.log.debug("Using options: " + JSON.stringify(options));
+
+        var template = generateTemplateSimple(),
+            configuraton = generateConfiguration(grunt, options);
+
+        if (options.browserify) {
+            template = generateTemplateBrowserified();
+        }
+
+        grunt.log.debug("Used template: " + template);
 
 
-    var generatedContent = grunt.template.process(template, {
-                                                              data: {
-                                                                "module": options.export_module,
-                                                                "variable": options.export_variable,
-                                                                "config": generateConfiguration()
-                                                              }
-                                                            }
-                                                          );
+        var generatedContent = grunt.template.process(template, {
+                data: {
+                    "module": options.export_module,
+                    "variable": options.export_variable,
+                    "config": generateConfiguration()
+                }
+            }
+        );
 
-        grunt.file.write(options.export_dest, generatedContent);
+        fs.writeFile(options.export_dest, generatedContent);
         grunt.log.ok('Generated ' + options.export_dest);
-  });
+    });
 };
